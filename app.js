21bb27a117d9c90fdd92b60aebb99ed14aa3f5b7
hitallocavas <hcs3@cin.ufpe.br>
2020-07-21 11:53:33 -0300

    Conserto de app para liberacao de cors

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var cors = require('cors');
 var roomController = require('./routes/roomController');
 
 var app = express();
-app.use(cors);
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
