6e311fff05b6852a57ccc6936008090a516e51f3
hitallocavas <hcs3@cin.ufpe.br>
2020-07-21 11:49:48 -0300

    adicionando liberaçao de cors

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var roomController = require('./routes/roomController');
 
 var app = express();
+app.use(cors);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
